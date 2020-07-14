b06a928d4cad0eb1bf8d99c05fef7162b13aa845
TyranReacts <s.mossa1990@gmail.com>
2020-07-14 15:42:21 +0200

    Implemented a test with Jest (simple rendering test)

---
+++ src/App.spec.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+import GuessCount from './GuessCount'
+
+describe('<App />', () => {
+	it('renders without crashing', () => {
+		const div = document.createElement('div') 	
+		ReactDOM.render(<App />, div)
+	})
+})
+
+
