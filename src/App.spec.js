70d31ef2937612ba7c8c8e1c547d1e95da48b669
TyranReacts <s.mossa1990@gmail.com>
2020-07-15 14:30:38 +0200

    Added comments to setupTests.js and App.spec.js

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { expect } from 'chai' 
+import { expect } from 'chai' // to replace 'expect' of jest
 
 import App from './App'
 //import GuessCount from './GuessCount'
@@ -13,6 +13,10 @@ describe('<App />', () => {
 
 	it('compares 2 objects', () => {
 		expect({name: 'Joe'}).to.equal({name: 'Jane'})
+	})
+
+	it('is nulle', () => {
+		expect(null).to.be.nul(); // thanks to dirty chai I can write properties like methods, which triggers TypeError if there is a typo
 	})
 })
 
--- src/setupTests.js
+++ src/setupTests.js
@@ -4,7 +4,7 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 import chai from 'chai'
-import dirtyChai from 'dirty-chai'
-import createChaiJestDiff from 'chai-jest-diff'
+import dirtyChai from 'dirty-chai' // allows to write assertion properties of chai like methods (to trigger explicit error if typo)
+import createChaiJestDiff from 'chai-jest-diff' // factory for displaying Assertion difference (when test fails) same as for jest
 
 chai.use(dirtyChai).use(createChaiJestDiff())
