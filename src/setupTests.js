12de9d49fe9b542f82b90669a4ded4495a85afd7
TyranReacts <s.mossa1990@gmail.com>
2020-07-16 01:26:38 +0200

    Set up chai, enzyme + added jest test for event

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,17 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import chai from 'chai'
+import dirtyChai from 'dirty-chai'
+import createChaiJestDiff from 'chai-jest-diff'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import createChaiEnzyme from 'chai-enzyme'
+
+
+chai
+    .use(dirtyChai)
+    .use(createChaiJestDiff())
+    .use(createChaiEnzyme())
+
+Enzyme.configure({ adapter: new Adapter()})
