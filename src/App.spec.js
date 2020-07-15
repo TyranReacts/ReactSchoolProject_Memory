1d7bac399122ffbfcc874fd5d96aa55f411168a0
TyranReacts <s.mossa1990@gmail.com>
2020-07-15 11:40:31 +0200

    Installed harnais chai along with chai-jest-diff

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { expect } from 'chai' 
 
 import App from './App'
-import GuessCount from './GuessCount'
+//import GuessCount from './GuessCount'
 
 describe('<App />', () => {
 	it('renders without crashing', () => {
 		const div = document.createElement('div') 	
 		ReactDOM.render(<App />, div)
+	})
+
+	it('compares 2 objects', () => {
+		expect({name: 'Joe'}).to.equal({name: 'Jane'})
 	})
 })
 
