c8b4004ff652c17b8fcc9ea453b104dc30cad8a0
TyranReacts <s.mossa1990@gmail.com>
2020-07-17 06:47:32 +0200

    Installed harnais chai enzyme and sinon

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,18 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import chai from 'chai'
+import dirtyChai from 'dirty-chai'
+import createChaiJestDiff from 'chai-jest-diff'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import createChaiEnzyme from 'chai-enzyme'
+import sinonChai from 'sinon-chai'
+
+chai
+    .use(dirtyChai)
+    .use(createChaiJestDiff())
+    .use(createChaiEnzyme())
+    .use(sinonChai)
+
+Enzyme.configure({ adapter: new Adapter()})
